41935025ee9b707791f71d4462e30f1d0a1c504c
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2023-12-06 14:57:32 +0200

    2&3 tasks

--- index.js
+++ index.js
@@ -15,3 +15,64 @@ while (currentUSD <= maxUSD) {
   console.log(result);
   currentUSD += 10;
 }
+
+console.log(isNaN(null));
+
+/* 2. Юзер вводить число. З'ясувати, чи є воно простим.
+ Простим називається ціле число, більше ніж 1, яке не має інших дільників, крім 1 і себе(1 не є простим).*/
+
+function isPrime(number) {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let input = prompt(`Enter number to check if it's prime.`);
+
+while (!input?.trim() || isNaN(input)) {
+  input = prompt('Enter valid number');
+  if (input === null) {
+    break;
+  }
+}
+if (input === null) {
+  alert('Canceled');
+} else {
+  alert(
+    `${isPrime(+input) ? `The number ${+input} is prime` : `The number ${+input} is not prime`} `
+  );
+}
+
+/* Юзер вводить число. 
+Визначити, чи можна одержати це число шляхом зведення числа 3 в деякий цілий ступінь. 
+Наприклад, числа 9, 81 можна отримати, бо це 3^2 і 3^4, а 13 - не можна.*/
+
+let inputNum = prompt(`Enter number to check if it's obtained by 3 raised to power of some exponent`);
+
+while (!inputNum?.trim() || isNaN(inputNum)) {
+  inputNum = prompt('Enter valid number');
+  if (inputNum === null) {
+    break;
+  }
+}
+if (inputNum === null) {
+  alert('Canceled');
+} else {
+  let number = parseInt(inputNum);
+  let exponent = 0;
+
+  while (Math.pow(3, exponent) < number) {
+    exponent++;
+  }
+  alert(
+    Math.pow(3, exponent) === number
+      ? `The number ${number} is the result of raising 3 to the power of ${exponent}`
+      : `This number cannot be obtained by raising the number 3 to some whole power.`
+  );
+}
